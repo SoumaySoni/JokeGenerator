bac30352ccbbbb146c0c10aa02ad0870f59ff58c
SoumaySoni <soumaysoni1687@gmail.com>
2024-05-02 13:07:24 +0530

    second commit

--- src/Components/Jokes.js
+++ src/Components/Jokes.js
@@ -1,16 +1,13 @@
 // Joke.js
 import './Joke.css';
 import React from "react";
-
 import Button from "./Button";
 
 const Joke = () => {
   const [Joke, setJoke] = React.useState("");
-
   const fetchApi = () => {
     fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
       .then((res) => res.json())
-
       .then((data) => setJoke(data.joke));
   };
 
